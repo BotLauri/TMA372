f94f3c19b0be432ae60a02df15d9949775d816ee
BotLauri <31406044+BotLauri@users.noreply.github.com>
2022-02-07 20:06:15 +0100

    問題１．２は終わった。今は次の問題決めたいと思います。

--- Exercise1_1.m
+++ Exercise1_1.m
@@ -9,7 +9,7 @@ f = @fcn;
 Th = [0, 1/6, 1/3, 1/2, 2/3, 5/6, 1];
 %Th = [0, 1/4, 1/2, 3/4, 1];
 syms x
-v = hatFunction(Th, x);
+v = HatFunction(Th, x);
 
 % Calculation of stiffness matrix and load vector. 
 A = zeros(length(Th) - 2, length(Th) - 2);
@@ -67,7 +67,7 @@ function f = fcn(x)
     f = 6*x;
 end
 
-function phi = hatFunction(Th, x)
+function phi = HatFunction(Th, x)
     phi = [];
     for i = 1:(length(Th) - 2)
         phi = [phi, triangularPulse(Th(i), (Th(i)+Th(i+2))/2, Th(i+2), x)];
--- Exercise1_2.m
+++ Exercise1_2.m
@@ -9,37 +9,26 @@ f = @fcn;
 Th = [0, 1/6, 1/3, 1/2, 2/3, 5/6, 1];
 %Th = [0, 1/4, 1/2, 3/4, 1];
 syms x
-lambda = lagrangePolynomial(Th, x);
+lambda = LagrangePolynomial(Th, x);
 
-% Calculation of stiffness matrix and load vector. FIX THESE! Formula?
-A = zeros(length(Th) - 2, length(Th) - 2);
+% Calculation of stiffness matrix and load vector. 
+A = zeros(length(Th)-2, length(Th)-2);
+lambdaDiff = diff(lambda);
 for i = 1:length(A)
     for j = 1:length(A)
-        if abs(i - j) < 2
-            if (i == j)
-                h1 = Th(i+1) - Th(i);
-                h2 = Th(i+2) - Th(i+1);
-                A(i,j) = 1/h1 + 1/h2;
-            else
-                h2 = Th(i+1) - Th(i);
-                A(i,j) = -1/h2;
-            end
-        end
+        A(i,j) = int(lambdaDiff(i+1)*lambdaDiff(j+1), 0, 1);
     end
 end
-b = zeros(length(Th) - 2, 1);
-for i = 2:(length(b)+1)
-    hi = Th(i) - Th(i-1);
-    firstTerm = int(f(x)*(x - Th(i-1))/hi, Th(i-1), Th(i));
-    hiPlusOne = Th(i+1) - Th(i);
-    secondTerm = int(f(x)*(Th(i+1) - x)/hiPlusOne, Th(i), Th(i+1));
-    b(i-1) = firstTerm + secondTerm;
+b = zeros(length(Th)-2, 1);
+for i = 1:length(b)
+    b(i) = int(f(x)*lambda(i+1), 0, 1);
 end
 
 % Finally solve the matrix equation.
 xi = linsolve(A, b);
-un = xi'.*lambda;
+un = xi'.*lambda(2:(length(Th)-1));
 disp(lambda)
+disp(xi)
 
 toc
 
@@ -49,15 +38,15 @@ for i = 1:length(Th)
     fplot(lambda(i), [0 1])
 end
 title('Plot of the Lagrange polynomials.')
-legend('\lambda_1(x)', '\lambda_2(x)', '\lambda_3(x)', '\lambda_4(x)', '\lambda_5(x)', '\lambda_6(x)', '\lambda_7(x)')
+legend('\lambda_0(x)', '\lambda_{1/2}(x)', '\lambda_1(x)', '\lambda_{3/2}(x)', '\lambda_2(x)', '\lambda_{5/2}(x)', '\lambda_3(x)')
 xlabel('x')
 hold off
 
-%% Plot of the solution and the cG(1) approximation. 
+%% Plot of the solution and the cG(2) approximation. 
 hold on
 fplot(x - x^3, [0 1])
 fplot(sum(un), [0 1])
-title('Plot of the exact solution, u(x), and the approximation cG(1), u_n(x).')
+title('Plot of the exact solution, u(x), and the approximation cG(2), u_n(x).')
 legend('u(x)', 'u_n(x)')
 xlabel('x')
 hold off
@@ -67,16 +56,25 @@ function f = fcn(x)
     f = 6*x;
 end
 
-function lambda = lagrangePolynomial(Th, x)
-    lambda = []; q = length(Th);
+function lambda = LagrangePolynomial(Th, x)
+    lambda = [];
+    q = length(Th);
+    h = Th(1+2) - Th(1);
     for i = 1:q
-        temp = 1;
-        for j = 1:q
-            if (i ~= j)
-                temp = temp * (x - Th(j))/(Th(i) - Th(j));
-            end
+        if (i == 1) % Special case.
+            int = 2*(Th(i+1) - x)*(Th(i+2) - x)/(Th(i+2) - Th(i))^2;
+            temp = piecewise(x < Th(i+2), int, x > Th(i+2), 0);
+        elseif (i == q) % Special case.
+            int = 2*(x - Th(i-2))*(x - Th(i-1))/(Th(i) - Th(i-2))^2;
+            temp = piecewise(x > Th(i-2), int, x < Th(i-2), 0);
+        elseif (mod(i, 2) ~= 0) % Whole number. 
+            int1 = 2*(x - Th(i-2))*(x - Th(i-1))/(Th(i) - Th(i-2))^2;
+            int2 = 2*(Th(i+1) - x)*(Th(i+2) - x)/(Th(i+2) - Th(i))^2;
+            temp = piecewise(Th(i-2) < x < Th(i), int1, Th(i) < x < Th(i+2), int2, x < Th(i-2), 0, x > Th(i+2), 0);
+        else % Half number. 
+            int = 4*(x - Th(i-1))*(Th(i+1) - x)/(2*(Th(i+1) - Th(i)))^2;
+            temp = piecewise(Th(i-1) < x < Th(i+1), int, x < Th(i-1), 0, x > Th(i+1), 0);
         end
         lambda = [lambda, temp];
     end
 end
- 
